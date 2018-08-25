6994bdfe81cfa7f69cc78ec1463be882b1576c40
田中 友磨 <yuma.tanaka1129@gmail.com>
2018-08-26 00:25:16 +0900

    Rename to card.

--- SwiftTdd/SwiftTddTests/SwiftTddTests.swift
+++ SwiftTdd/SwiftTddTests/SwiftTddTests.swift
@@ -47,11 +47,14 @@ class SwiftTddTests: XCTestCase {
     }
     
     func testCardNotation() {
-        let card1 = Card(suit: .heart, rank: .three)
-        XCTAssertEqual(card1.notation, "3♥")
         
-        let card2 = Card(suit: .spade, rank: .jack)
-        XCTAssertEqual(card2.notation, "J♠")
+        var card: Card
+        
+        card = Card(suit: .heart, rank: .three)
+        XCTAssertEqual(card.notation, "3♥")
+        
+        card = Card(suit: .spade, rank: .jack)
+        XCTAssertEqual(card.notation, "J♠")
     }
     
 }
